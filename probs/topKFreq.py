225b0ea1ae71c9bf4cbce8086dd2c5be2039f5a1
Tony De La Nuez <a.dela@wustl.edu>
2017-08-11 21:41:00 -0700

    Google Code Jam 1 complete

--- probs/topKFreq.py
+++ probs/topKFreq.py
@@ -1,9 +1,11 @@
+# return the top frequency of a number
 nums = [1,1,1,2,2,3,7,8,5,5,3,3,3,4,2,9,9,9,6]
 k = 2
 #return[1,2]
 
 freq = {}
 for val in nums:
+	# 
 	if(val not in freq):
 		freq[val] = 1
 	else:
